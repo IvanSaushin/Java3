0a2fcdb3d79a5a4c4bda55bec048c9c406f6478c
Ivan Saushin <ivan.saushin@gmail.com>
2020-07-09 12:15:12 +0400

    Merge branch 'master' of C:\Java\Java3 with conflicts.

--- src/Generics/task3/Box.java
+++ src/Generics/task3/Box.java
@@ -1,17 +1,38 @@
 package Generics.task3;
 
+import java.util.ArrayList;
+
 public class Box<B extends Fruit> {
-    private B element;
+    private ArrayList<B> box;
 
-    public B getThings() {
-        return element;
+    public ArrayList<B> getBoxContent() {
+        return box;
     }
 
-    public Box (B element) {
-        this.element = element;
+    public Box (ArrayList<B> box) {
+        this.box = box;
     }
 
-    public Box() {
+    public void addFruit(B fruit) {
+        box.add(fruit);
+    }
 
+    public void compareBox(Box fruit) {
+        float dif = this.getWeightBox() - fruit.getWeightBox();
+        if (dif>0) {
+            System.out.println("Первая коробка больше");
+        }
+        else if (dif<0) System.out.println("Первая коробка меньше");
+        else System.out.println("Коробки равны");
+    }
+
+    public void moveToBox(Box<?super B> box) { // вопрос 2
+        box.getBoxContent().addAll(this.box);
+        this.box.clear();
+    }
+
+    public float getWeightBox() {
+        if (box.size() == 0) return 0;
+        else return (float) box.size() * this.getBoxContent().get(0).getWeightFruit();
     }
 }
